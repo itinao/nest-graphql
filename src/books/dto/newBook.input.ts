365a00e1d944aa7f15b19e1b30d56feddedd0054
itinao <itinao@gmail.com>
2021-11-10 15:31:29 +0900

    fix typo

--- src/books/dto/newBook.input.ts
+++ src/books/dto/newBook.input.ts
@@ -2,7 +2,7 @@ import { Field, InputType, Int } from '@nestjs/graphql';
 import { Max, MaxLength, Min } from 'class-validator';
 
 @InputType()
-export class NewBookInput {
+export class newBookInput {
  @Field()
  @MaxLength(30)
  title: string;

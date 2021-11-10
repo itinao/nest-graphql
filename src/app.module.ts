d5f9296aaff6405fd75839645bc74dae1b90472e
itinao <itinao@gmail.com>
2021-11-10 14:20:29 +0900

    generate books

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
